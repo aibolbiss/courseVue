b93967403b0a10dafd20a089b83d68e2aa90f166
kapayji <kapayji@gmail.com>
2021-04-10 21:00:00 +0300

    DZ_10.04

---
+++ vue-figma/pages/page_1.vue.js
@@ -0,0 +1,3 @@
+var Page_1 = {
+    template: '<div><h1>Page_1</h1><p>page number 1</p></div>',
+};
---
+++ vue-figma/pages/page_2.vue.js
@@ -0,0 +1,3 @@
+var Page_2 = {
+    template: '<div><h1>Page_2</h1><p>page number 2</p></div>',
+};
